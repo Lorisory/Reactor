3785c7fc369aaf54b48b9b3c9b190d8d9f003bf5
Tomáš <tiger.pilnas@seznam.cz>
2024-10-09 20:55:03 +0200

    Refactoring so the components dont look like shit

---
+++ src/DearMe/DearMeSubpageContainer.tsx
@@ -0,0 +1,17 @@
+import {Outlet} from "react-router-dom";
+
+export default function DearMeSubpageContainer() {
+  return (<div
+    style={{
+      border: 'dashed red 3px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'top',
+      flexWrap: 'wrap', // Allows wrapping items
+      height: '100vh',
+    }}
+  >
+    {/* Outlet will render the respective subpage content */}
+    <Outlet/>
+  </div>)
+}

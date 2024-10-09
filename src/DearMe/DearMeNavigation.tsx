63bc039b2484e833056bee0fe1b5094bd956ec26
Tomáš <tiger.pilnas@seznam.cz>
2024-10-09 20:55:03 +0200

    Refactoring so the components dont look like shit

---
+++ src/DearMe/DearMeNavigation.tsx
@@ -0,0 +1,31 @@
+import {useNavigate} from "react-router-dom";
+
+export default function DearMeNavigation() {
+  const navigate = useNavigate();
+  return (<div style={{
+    display: 'flex',
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  }}>
+    <button
+      className="DearMeToolbarButton"
+      onClick={() => navigate('/dear-me/1-week')}
+    >
+      1 Week
+    </button>
+    <button
+      className="DearMeToolbarButton"
+      onClick={() => navigate('/dear-me/1-month')}
+    >
+      1 Month
+    </button>
+    <button
+      className="DearMeToolbarButton"
+      onClick={() => navigate('/dear-me/1-year')}
+    >
+      1 Year
+    </button>
+  </div>)
+}
--- src/DearMe/DearMePage.tsx
+++ src/DearMe/DearMePage.tsx
@@ -1,51 +1,14 @@
 import './DearMeStyles.css';
-import { useNavigate, Outlet } from 'react-router-dom';
+import DearMeNavigation from "./DearMeNavigation";
+import DearMeSubpageContainer from "./DearMeSubpageContainer";
 
 function DearMePage() {
-  const navigate = useNavigate();
+
 
   return (
     <div style={{ width: '100%' }}>
-      <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        textAlign: 'center',
-        justifyContent: 'center',
-        width: '100%',
-      }}>
-        <button
-          className="DearMeToolbarButton"
-          onClick={() => navigate('/dear-me/1-week')}
-        >
-          1 Week
-        </button>
-        <button
-          className="DearMeToolbarButton"
-          onClick={() => navigate('/dear-me/1-month')}
-        >
-          1 Month
-        </button>
-        <button
-          className="DearMeToolbarButton"
-          onClick={() => navigate('/dear-me/1-year')}
-        >
-          1 Year
-        </button>
-      </div>
-
-      <div
-        style={{
-          border: 'dashed red 3px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'top',
-          flexWrap: 'wrap', // Allows wrapping items
-          height: '100vh',
-        }}
-      >
-        {/* Outlet will render the respective subpage content */}
-        <Outlet />
-      </div>
+      <DearMeNavigation />
+      <DearMeSubpageContainer />
     </div>
   );
 }
